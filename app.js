99316a0be7e00b40a36bed6462d7ac75893d5e5f
Will McCloy <william.mccloy@googlemail.com>
2021-11-02 11:36:45 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const ejs = require("ejs");
+const data = require("./data.json");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+
+
+
+// Home route
+app.get("/", (req, res) => {
+    let outData = [];
+    data.forEach((cat) => {
+        outData.push({
+            title: cat.title,
+            data: cat.timeframes.daily
+        })
+    });
+    res.render("home", {
+        data: outData,
+        timeframe: "Yesterday"
+    });
+});
+
+app.get("/daily", (req, res) => {
+    let outData = [];
+    data.forEach((cat) => {
+        outData.push({
+            title: cat.title,
+            data: cat.timeframes.daily
+        })
+    });
+    res.render("home", {
+        data: outData,
+        timeframe: "Yesterday"
+    });
+});
+
+app.get("/weekly", (req, res) => {
+    let outData = [];
+    data.forEach((cat) => {
+        outData.push({
+            title: cat.title,
+            data: cat.timeframes.weekly
+        })
+    });
+    res.render("home", {
+        data: outData,
+        timeframe: "Last week"
+    });
+});
+
+app.get("/monthly", (req, res) => {
+    let outData = [];
+    data.forEach((cat) => {
+        outData.push({
+            title: cat.title,
+            data: cat.timeframes.monthly
+        })
+    });
+    res.render("home", {
+        data: outData,
+        timeframe: "Last month"
+    });
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+});
